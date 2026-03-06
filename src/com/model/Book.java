9b34298246b2e6250c5d5d6a8eeed246a25a9ed9
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-06 09:47:55 +0300

    Book Class
    It keeps track of the books' basic characteristics and condition.

--- src/com/model/Book.java
+++ src/com/model/Book.java
@@ -1,4 +1,37 @@
 package com.model;
 
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String isbn, String title, String author){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("ISBN: %s | Kitap: %s | Yazar: %s | Durum: %s",
+        isbn,title,author,(isBorrowed ? "Ödünç Verildi" : "Mevcut"));
+    }
 }
