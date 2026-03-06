7cdbd61627fa5cb34703f296c071a135ed58783d
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-06 10:01:17 +0300

    Member Class
    It represents the library members.

--- src/com/model/Member.java
+++ src/com/model/Member.java
@@ -1,4 +1,19 @@
 package com.model;
 
 public class Member {
+    private int id;
+    private String name;
+
+    public Member(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
