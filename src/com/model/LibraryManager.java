390cf14b40de5804d0c4eb4611d6c50fc521bfcf
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-06 10:28:58 +0300

    LibraryManager Class
    This is where we manage data using ArrayLists, and where the actual business logic takes place.

--- src/com/model/LibraryManager.java
+++ src/com/model/LibraryManager.java
@@ -1,4 +1,34 @@
 package com.model;
 
+import java.util.ArrayList;
+
 public class LibraryManager {
+    private ArrayList<Book> bookList = new ArrayList<>();
+
+    public void addBook(Book book) {
+        bookList.add(book);
+        System.out.println("Sisteme eklendi: " + book.getTitle());
+    }
+
+    public void listBooks() {
+        System.out.println("\n--- Güncel Kütüphane Kitap Listesi ---");
+        for (Book book : bookList) {
+            System.out.println(book);
+        }
+    }
+
+    public void borrowBook(String isbn) {
+        for (Book book : bookList) {
+            if (book.getIsbn().equals(isbn)) {
+                if (!book.isBorrowed()) {
+                    book.setBorrowed(true);
+                    System.out.println(book.getTitle() + " başarıyla ödünç verildi.");
+                } else {
+                    System.out.println("Uyarı: Bu kitap başka birine ödünç verilmiştir.");
+                }
+                return;
+            }
+        }
+        System.out.println("Hata: Kitap bulunamadı.");
+    }
 }
